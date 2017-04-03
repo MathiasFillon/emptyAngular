3c8b4a063e1cd37f5f184f01116872c8f007ca58
MathF <mathias.fillon@gmail.com>
2017-04-03 11:07:07 +0200

    first commit

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,14 @@
 // MAIN CONTROLLER
-function mainController($scope) {
-    
-}
+angular.module('app', [])
+
+    .controller('mainController', function($scope){
+
+        $scope.objets = [];
+
+        $scope.add = function() {           
+          $scope.objets.push({name: $scope.addObjet.name, last: $scope.addObjet.last, mail: $scope.addObjet.mail});
+        };
+    });
+
+
+
